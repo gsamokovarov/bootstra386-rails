988af6964e125a367c7da8eb197505d2c466a548
Genadi Samokovarov <gsamokovarov@gmail.com>
2014-07-29 11:36:46 +0200

    Remove the minified files
    Use the asset pipeline to do that.

--- lib/bootstra386/rails/engine.rb
+++ lib/bootstra386/rails/engine.rb
@@ -1,5 +1,13 @@
 module Bootstra386
   module Rails
-    Engine = Class.new(::Rails::Engine)
+    class Engine < ::Rails::Engine
+      initializer 'bootstra386.assets.precompile' do |app|
+        fonts_path = root.join('vendor', 'assets', 'fonts').to_s
+        if app.config.assets.paths.exclude?(fonts_path)
+          app.config.assets.paths << fonts_path
+        end
+        app.config.assets.precompile << /Fixedsys500c\.(?:eot|otf|svg|ttf|woff)$/
+      end
+    end
   end
 end
